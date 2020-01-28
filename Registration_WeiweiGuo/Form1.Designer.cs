d3732c2903abb05d0f6f843c3f93611b9dc2db95
Weiwei Guo <6151717@johnabbottcollege.net>
2020-01-27 20:22:53 -0500

    Add project files.

---
+++ Registration_WeiweiGuo/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Registration_WeiweiGuo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.lblCountrty = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(145, 44);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(244, 37);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 1;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.FirstName_Mandatory_Input);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(244, 63);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 3;
+            this.txtLastName.TextChanged += new System.EventHandler(this.LastName_Mandatory_Input);
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(145, 70);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(244, 89);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 5;
+            this.txtEmail.TextChanged += new System.EventHandler(this.Email_Mandatory_Input);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(145, 96);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(244, 115);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(145, 122);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 6;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(244, 141);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 9;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(145, 148);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 8;
+            this.lblCity.Text = "City";
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(244, 167);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 11;
+            this.txtPostalCode.TextChanged += new System.EventHandler(this.PostalCode_Validation);
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Location = new System.Drawing.Point(145, 174);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(64, 13);
+            this.lblPostalCode.TabIndex = 10;
+            this.lblPostalCode.Text = "Postal Code";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(244, 193);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 20);
+            this.txtCountry.TabIndex = 13;
+            // 
+            // lblCountrty
+            // 
+            this.lblCountrty.AutoSize = true;
+            this.lblCountrty.Location = new System.Drawing.Point(145, 200);
+            this.lblCountrty.Name = "lblCountrty";
+            this.lblCountrty.Size = new System.Drawing.Size(46, 13);
+            this.lblCountrty.TabIndex = 12;
+            this.lblCountrty.Text = "Countrty";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(244, 219);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 15;
+            this.txtPhone.TextChanged += new System.EventHandler(this.Phone_Validation);
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(145, 226);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(38, 13);
+            this.lblPhone.TabIndex = 14;
+            this.lblPhone.Text = "Phone";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(357, 310);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 16;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(478, 310);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 17;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.lblCountrty);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label lblCountrty;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ Registration_WeiweiGuo/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Registration_WeiweiGuo
+{
+    public partial class Form1 : Form
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       //First name mandatory input
+        private void FirstName_Mandatory_Input(object sender, EventArgs e)
+        {
+            if (txtFirstName.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter your first name!");
+                return;
+            }
+        }
+
+        //Last name mandatory input
+        private void LastName_Mandatory_Input(object sender, EventArgs e)
+        {
+             if (txtLastName.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter your last name!");
+                return;
+            }          
+        }
+
+        //Email name mandatory input
+        private void Email_Mandatory_Input(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter your email!");
+                return;
+            }
+        }
+
+        //Post code validation
+        private void PostalCode_Validation(object sender, EventArgs e)
+        {
+            string postCodePattern = @"^\d{3}\s?\d{3}$";
+            bool isPostCodeValid = Regex.IsMatch(txtPostalCode.Text, postCodePattern);
+            if (!isPostCodeValid)
+            {
+                MessageBox.Show("Please enter a valid postal code");
+            }
+        }
+
+        //Phone validation
+        private void Phone_Validation(object sender, EventArgs e)
+        {
+            string phonePattern = @"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$";
+            bool isPhoneValid = Regex.IsMatch(txtPhone.Text, phonePattern);
+            if (!isPhoneValid)
+            {
+                MessageBox.Show("Please enter a valid phone code");
+            }
+        }
+
+        //Click button reset
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+        }
+
+        //Click button submit
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+           //show message
+            MessageBox.Show(txtFirstName.Text + ", " + txtLastName.Text + ", " + txtEmail.Text + ", " 
+                            + txtAddress.Text + ", " + txtCity.Text + ", " + txtPostalCode.Text + ", " 
+                            + txtCountry.Text + ", " + txtPhone.Text);
+
+       
+            //write to file
+            using (fs = new FileStream("registorInfo.txt", FileMode.Append, FileAccess.Write))
+            {
+                using (sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(txtFirstName.Text + ", " + txtLastName.Text + ", " + txtEmail.Text + ", "
+                            + txtAddress.Text + ", " + txtCity.Text + ", " + txtPostalCode.Text + ", "
+                            + txtCountry.Text + ", " + txtPhone.Text);
+                    sw.Flush();
+                    sw.Close();
+                    fs.Close();
+                }
+
+                //clear input
+                txtFirstName.Clear();
+                txtLastName.Clear();
+                txtEmail.Clear(); 
+                txtAddress.Clear(); 
+                txtCity.Clear();
+                txtPostalCode.Clear();
+                txtCountry.Clear();
+                txtPhone.Clear();
+            }
+
+            
+        }
+
+       
+
+    }
+}
